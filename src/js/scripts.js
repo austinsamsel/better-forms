1da8863403c03e5aa4ca879ebe7a3466c3f66ca5
austin samsel <austinsamsel@gmail.com>
2016-02-07 18:17:44 -0500

    remove red on first tab for age and install + add styles to chosen js for select

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,22 @@
 (function ($, window, document, undefined) {
 
+  // on the fly validation for age input
   $(".js-age").on("change keyup", function() {
-    if ($(this).val() < 1 || $(this).val() > 130 ){
+    //TODO: break this out into fuctions later.
+    if ($(this).val() < 1 && $(this).val().length != 0 || $(this).val() > 130 && $(this).val().length != 0 ){
       console.log('yo wtf');
       $(this).addClass('error');
     } else{
       $(this).removeClass('error');
     }
   });
+
+  // init chosen for all select forms
+  $("select").chosen();
+
+  $('select').first().trigger('chosen:open');
+
+
+  //
 
 })(jQuery, window, document);
