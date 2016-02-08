64b0c82fcf641af58b3b5490e598f0ccb40da9c8
austin samsel <austinsamsel@gmail.com>
2016-02-08 17:19:07 -0500

    fail sample

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 var expect = chai.expect;
 var should = chai.should();
 
-it('1 should equal 1', function() {
-  expect(1).to.equal(1);
+describe("basic stuff", function(){
+  it('1 should equal 1', function() {
+    expect(1).to.equal(1);
+  });
+  it('should have a title', function(){
+    assert.equal($(".panel-title").html(), "family health history.")
+  })
 });
